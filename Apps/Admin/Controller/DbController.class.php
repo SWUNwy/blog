7f930252024f03d26393321b6c784cf75f018d69
wangyu <swunwy@163.com>
2017-11-13 23:04:38 +0800

    update

---
+++ Apps/Admin/Controller/DbController.class.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Admin\Controller;
+
+use Think\Controller;
+
+/**
+ * Description of DbController
+ *
+ * @author Administrator
+ */
+class DbController extends Controller {
+    //put your code here
+}
---
+++ Apps/Admin/Model/DbModel.class.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Admin\Model;
+
+use Think\Model;
+
+/**
+ * Description of DbModel
+ *
+ * @author Administrator
+ */
+class DbModel extends Model {
+    //put your code here
+}
